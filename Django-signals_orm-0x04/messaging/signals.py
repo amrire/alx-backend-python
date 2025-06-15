0b97a809bd64b360d4bff964e690da9b979a4d6f
x86skwizer <yassineamrire@protonmail.com>
2025-06-15 13:31:53 +0100

    Create a Signal for Logging Message Edits

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,23 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.id is None:
+        return  # Skip new messages
+
+    try:
+        original = Message.objects.get(id=instance.id)
+        if original.content != instance.content:
+            MessageHistory.objects.create(
+                message=original,
+                old_content=original.content
+            )
+            instance.edited = True  # Mark message as edited
+    except Message.DoesNotExist:
+        pass
+
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
