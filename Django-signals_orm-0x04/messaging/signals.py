60a014cec8a9bda8a3ce05eadead109d9dea9589
x86skwizer <yassineamrire@protonmail.com>
2025-06-15 14:01:43 +0100

    Use Signals for Deleting User-Related Data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
+from django.contrib.auth.models import User
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -25,3 +26,16 @@ def log_message_edit(sender, instance, **kwargs):
             instance.edited = True  # Mark message as edited
     except Message.DoesNotExist:
         pass
+    
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete messages sent or received by the user
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete any notifications linked to the user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete message histories related to messages from/to this user
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
